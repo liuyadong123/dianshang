c6fd17fa687189031b5d56601eafa8cbf7f19bce
dong <947212712@qq.com>
2018-12-27 19:12:01 +0800

    hhahhahha

--- day02/src/main/java/com/example/day02/modle/user/IRegModle.java
+++ day02/src/main/java/com/example/day02/modle/user/IRegModle.java
@@ -1,4 +1,9 @@
 package com.example.day02.modle.user;
 
+import java.util.HashMap;
+
 public interface IRegModle {
+
+   void reg(HashMap<String,String> params);
+
 }
--- day02/src/main/java/com/example/day02/view/user/IRegView.java
+++ day02/src/main/java/com/example/day02/view/user/IRegView.java
@@ -1,5 +1,5 @@
 package com.example.day02.view.user;
 
 public class IRegView {
-    
+
 }
